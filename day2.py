2576fb249bc64849e5ce9d664afa5cdac8032314
root <root@station101.example.com>
2018-11-14 14:38:28 +0000

    okey

--- day2.py
+++ day2.py
@@ -5,4 +5,7 @@ print   "Hello world"   # print string
 print  z
 print  "9999999"
 print type(x)
+s1="Hello"
+s2="world"
+print s1+" "+s2*5
 
---
+++ input.py
@@ -0,0 +1,11 @@
+s1=raw_input("enter any string :   ")    #  this is ask for string type input 
+s2=raw_input("enter any strng :  ")    #  this is ask for string type input 
+print  s1+s2
+
+#  typecasting ---  string -into  int 
+n1=raw_input("type any number   ")
+n2=raw_input("type any number   ")
+
+print   int(n1)+int(n2)
+
+
